c3ef2744aef39c32e80d16b24371adaff70eae7c
Makavura <makavuramughanga@gmail.com>
2021-01-26 09:03:49 +0300

    pre-cdn hosting chores

--- dist/mirror.js
+++ dist/mirror.js
@@ -4,11 +4,8 @@ Name data attributes to match Marketo's/Mirror form Input element's ids
 Give First Form a class name e.g formhelper-1 and a unique ID
 Give First Form button submit a unique ID
 Give second form an different class that mirrors the first form - e.g formhelper-2 
- 
- 
 Radio Buttons Require name to be similar if only one is to be selected, 
 if choice is being made between a different set, set a different name
- 
 On Change of Select, Multiple Select, Checkbox and Radio button change, input values will be reflected in the mirror form
 On Submit, Text, Number, Email & Password form input values will be fetched and filled 
 */
@@ -18,6 +15,61 @@ Fetch Value
 Set Value in Child Form
 */
 $(document).ready(function () {
+
+
+    /* 
+    Rabge based inputs
+   */
+    $('input[type="range"]').change(function (e) {
+
+        /* 
+        For Debugging Purposes
+        */
+        console.log($(this));
+        console.log($(this).data());
+        console.log($(this).parent());
+        console.log($(this).parent()[0]);
+        console.log($(this).parent()[0]["parentElement"]);
+        console.log($(this).parent()[0]["parentElement"]["id"]);
+        console.log($(this).parent()[0]["parentElement"]["className"]);
+        /* 
+        Variable Declaration
+        */
+        const _inputDataAttributes = $(this).data(); // data-* attributes
+        const _ = $(this).parent()[0]["parentElement"]["className"]; // current parent form className
+        const _length = _.length; // length of className
+        const _res = parseInt(_.slice((_length - 1)), 10); // parent form class suffix, to indicate next form that should be mirrored programmatically
+        const _parentFormClassName = _.slice(0, (_length - 1)); // parent form class prefix, to indicate next form that should be mirrored programmatically
+        const _mirrorFormClassName = _parentFormClassName + (_res + 1); // mirror form - form to submit values to 
+        var _mirrorForm = $(`.${_mirrorFormClassName} :input[type="radio"]`);
+        /* 
+        Check Mirror Form's element's id's to find the one that matches the data attributes of the current input
+        If there is a matching entry, 
+        Set the mirror form's input value to the current input
+        */
+        for (let i = 0; i < _mirrorForm.length; i++) {
+            for (const [key, value] of Object.entries(_inputDataAttributes)) {
+                if (_mirrorForm[i]["id"] == value) {
+
+                    /* 
+                      For Debugging Purposes
+                    */
+                    // console.log(_mirrorForm);
+                    // console.log(_mirrorForm[i]);
+                    // console.log(_mirrorForm[i]['id']);
+                    if ($(this).is(":checked")) {
+                        document.getElementById(`${_mirrorForm[i]['id']}`).checked = true;
+                    }
+                    else if ($(this).is(":not(:checked)")) {
+                        document.getElementById(`${_mirrorForm[i]['id']}`).checked = false;
+                    }
+                }
+            }
+        }
+
+    });
+
+
     /* 
      Radio based inputs
     */
