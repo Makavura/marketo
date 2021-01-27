f495149b418cc5d6ef08d1d2926fa95493bed731
Makavura <makavuramughanga@gmail.com>
2021-01-27 15:58:43 +0300

    ..

--- dist/select.input.js
+++ dist/select.input.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
      Selection based inputs
     */
     $('select').change(function (e) {
-        console.log($(this), e.target.value);
+        // console.log($(this), e.target.value);
 
         $('select').each(function (i, value) {
-            console.log(i, value);
+            // console.log(i, value);
         });
 
     });
