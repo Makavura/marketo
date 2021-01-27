fb8ef863b40e7f040f3ca37c342f9aacc7fbe0e6
Makavura <makavuramughanga@gmail.com>
2021-01-27 18:44:18 +0300

    --allow-empty

--- dist/select.input.js
+++ dist/select.input.js
@@ -7,19 +7,31 @@ $(document).ready(function () {
     */
     $('select').change(function (e) {
         $('select').each(function (i, value) {
+
             // console.log(i, value, e.target.id, $('select')[i]['id'], $('select').closest('form'));
             if ($('select')[i]['id'] == e.target.id) {
+
                 // console.log($('select')[i]['id'], e.target.id, $('select').length - (i +1));
                 // console.log($('select')[`${$('select').length - (1 +i)}`]);
-                if(i == ($('select').length/2)){
+                
+                if (i == ($('select').length / 2)) {
                     return;
                 }
-                const _ = (($('select').length/2) + i);
-                console.log(i);
-                console.log(_,$('select')[_].value, $('select')[i].value )
-                $(`${$('select')[_]['id']}`)
-                console.log($('select')[_]['id']);
+
+                const _ = (($('select').length / 2) + i);
+                // console.log(i);
+                // console.log(_, $('select')[_].value, $('select')[i].value)
+
+
+                $('select')[_].options
+
+                for (var i = 0; i < $('select')[_].options.length; i++){
+                    console.log($('select')[_].options[i]);
+                }
+                // $(`${$('select')[_]['id']}`)
+                // console.log($('select')[_]['option']);
             }
+
         });
     });
 
