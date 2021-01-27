6091a53808059574b36d4e4134becbb9e06c3d12
Makavura <makavuramughanga@gmail.com>
2021-01-27 17:29:21 +0300

    --allow-empty

--- dist/select.input.js
+++ dist/select.input.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
     /* 
      Selection based inputs
+
+     Find parent form class name
+     Check 
     */
     $('select').change(function (e) {
         $('select').each(function (i, value) {
-            console.log(i, value, e.target.id, $('select')[i]['id']);
+            console.log(i, value, e.target.id, $('select')[i]['id'], $('select').closest('form'));
             if ($('select')[i]['id'] == e.target.id) {
 
                 $('select')[$('select').length - i];
                 console.log(
                     $('select')[$('select').length - i]);
-
             }
         });
     });
