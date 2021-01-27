daaf49edbdf4c8730ee78811ad8462ef1b03ae16
Makavura <makavuramughanga@gmail.com>
2021-01-27 16:17:22 +0300

    feature/beta-release: reflect selection values

--- dist/select.input.js
+++ dist/select.input.js
@@ -3,19 +3,12 @@ $(document).ready(function () {
      Selection based inputs
     */
     $('select').change(function (e) {
-        // console.log($(this), e.target.value);
-
-        let _ = $('select').length;
-
-        let rest = _/2;
-
-
         $('select').each(function (i, value) {
-            if($('select')[i]['id'] == e.target.id){
-            console.log(i, value);
+            if ($('select')[i]['id'] == e.target.id) {
+                let mirrorIndex = $('select').length - i;
+                $('select')[mirrorIndex].value  = e.target.value;
             }
         });
-
     });
 
 });
