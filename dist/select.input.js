30b18c14d92a95a3884b4b836a5bf9b1d089dced
Makavura <makavuramughanga@gmail.com>
2021-01-27 17:31:09 +0300

    --allow-empty

--- dist/select.input.js
+++ dist/select.input.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
      Find parent form class name
      Check 
     */
-    $('select').change(function (e) {
+    $('#webflow :select').change(function (e) {
         $('select').each(function (i, value) {
             console.log(i, value, e.target.id, $('select')[i]['id'], $('select').closest('form'));
             if ($('select')[i]['id'] == e.target.id) {
