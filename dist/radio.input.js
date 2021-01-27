d9f7ef89b6c4df3509903ec15b94b5d79e98aaab
Makavura <makavuramughanga@gmail.com>
2021-01-27 13:26:48 +0300

    --allow-empty

--- dist/radio.input.js
+++ dist/radio.input.js
@@ -2,20 +2,22 @@ $(document).ready(function () {
     /* 
      Radio based inputs
     */
-   $('#webflow :input[type="radio"]').change(function(e){
-    $('#webflow :input[type="radio"]').each(function (value, i) {
-        /* 
-        Find index of radio button
-        Find the element in mirror form that matches this index
-        Mirror selection
-        */
-        if ($(this).is(":checked")) {
-            $('#mktoForm_1048 :input[type="radio"]')[i].checked = true;
-        }
-        else if ($(this).is(":not(:checked)")) {
-            $('#mktoForm_1048 :input[type="radio"]')[i].checked = true;
-        }
-    });
-   })
+    $('#webflow :input[type="radio"]').change(function (e) {
+        console.log($(this));
+        $('#webflow :input[type="radio"]').each(function (value, i) {
+            /* 
+            Find index of radio button
+            Find the element in mirror form that matches this index
+            Mirror selection
+            */
+            console.log($(this));
+            if ($(this).is(":checked")) {
+                $('#mktoForm_1048 :input[type="radio"]')[i].checked = true;
+            }
+            else if ($(this).is(":not(:checked)")) {
+                $('#mktoForm_1048 :input[type="radio"]')[i].checked = true;
+            }
+        });
+    })
 
 });
