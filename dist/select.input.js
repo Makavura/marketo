206b0a4c6c4fc3ad216f900d6130905249515569
Makavura <makavuramughanga@gmail.com>
2021-01-27 15:55:20 +0300

    --allow-empty

--- dist/select.input.js
+++ dist/select.input.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
      Selection based inputs
     */
     $('select').change(function (e) {
-        console.log($(this));
+        console.log($(this), e.target.value);
+
+        $('select').each(function (i, value) {
+            console.log(i, value);
+        });
+
     });
 
 });
