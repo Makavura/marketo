b3ec654b7ddf07c7696495864f58b78b6a539792
Makavura <makavuramughanga@gmail.com>
2021-01-28 14:39:47 +0300

    --allow-empty

--- dist/number.input.js
+++ dist/number.input.js
@@ -4,16 +4,12 @@ $(document).ready(function () {
     */
 
    document.getElementById("Phone-2").setAttribute('type', 'number');
+   document.getElementById("leadScore__pc").setAttribute('type', 'number');
 
     $('#webflow :input[type="number"]').change(function (e) {
         console.log($(this));
         $('#webflow :input[type="number"]').each(function (i, value) {
-            /* 
-            Find index of input element
-            Find the element in mirror form that matches this index
-            Mirror selection
-            */
-           console.log(value, i, $('#mktoForm_1048 :input[type="email"]')[i]);
+           
             if ($('#webflow :input[type="number"]')[i]['id'] == e.target.id) {
                     $('#mktoForm_1048 :input[type="number"]')[i].value = e.target.value;
             }
