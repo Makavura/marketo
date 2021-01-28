07ec7c6eb533df22af361ccac3a591c2248b3a7e
Makavura <makavuramughanga@gmail.com>
2021-01-28 13:33:46 +0300

    --allow-empty

--- dist/tel.input.js
+++ dist/tel.input.js
@@ -3,12 +3,15 @@ $(document).ready(function () {
      Tel based inputs
     */
     $('#webflow :input[type="tel"]').change(function (e) {
+        console.log(e.target, e.target.value);
+
         $('#webflow :input[type="tel"]').each(function (i, value) {
             /* 
             Find index of tel button
             Find the element in mirror form that matches this index
             Mirror selection
             */
+           console.log(i, value);
             if ($('#webflow :input[type="tel"]')[i]['id'] == e.target.id) {
                     $('#mktoForm_1048 :input[type="tel"]')[i].value = e.target.value;
             }
