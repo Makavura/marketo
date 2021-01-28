af2ff0cba13bd6943def5bcbb6845bbab10e37d9
Makavura <makavuramughanga@gmail.com>
2021-01-28 14:23:02 +0300

    beta-release

--- dist/final.submission.js
+++ dist/final.submission.js
@@ -1,67 +1,35 @@
 $(document).ready(function () {
     /* 
-       
        Submission event of first form
-
        */
     $("#webflow-submit").click(function (e) {
         e.preventDefault();
 
         /* 
-  Get all inputs from first form
-  */
+         Get all inputs from first form
+         */
 
-        let initialFormInputs = $("#webflow :input");
-        let mirrorFormInputs = $("#mktoForm_1048 :input");
-
+        let WFInputs = $("#webflow :input");
+        let MFInputs = $("#mktoForm_1048 :input");
 
         for (let i = 0; i < initialFormInputs.length; i++) {
-            /* 
-            Access their inner values
-            */
-            console.log($(`#${initialFormInputs[i]["id"]}`).attr('type'));
 
-            if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'email') {
-                /* 
-                Set email values
-                */
-            } else if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'text') {
-                /* 
-                Set text values
-                */
-            } else if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'password') {
-                /* 
-                Set password values
-                */
-            } else if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'number') {
-                /* 
-                Set number values
-                */
+            if (WFInputs[i]["type"] == 'text') {
+                MFInputs[i]["value"] = WFInputs[i]["value"];
+            } else if (WFInputs[i]["type"] == 'number') {
+                MFInputs[i]["value"] = WFInputs[i]["value"];
             }
-
 
         }
 
         document.querySelectorAll("textarea").forEach(function (value, index, parent) {
-
             if (index == ((document.querySelectorAll("textarea").length / 2))) {
                 return true;
             }
-
             document.querySelectorAll("textarea")[`${(document.querySelectorAll("textarea").length / 2)}`].value = document.querySelectorAll("textarea")[index].value;
-
         })
-        
-        console.log(mirrorFormInputs);
-        console.log(initialFormInputs);
 
-        _mirrorFormData = new FormData($("#mktoForm_1048"));
-        console.log(_mirrorFormData);
-
-
-        // let _FormData = new FormData($('#mktoForm_1048'));
-        // console.log(_FormData);
-
+        console.warn($('#mktoForm_1048'));
     });
 
 });
