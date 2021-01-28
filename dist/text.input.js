aed45830a1f63ea44fc6f97baff77e9b2723cdea
Makavura <makavuramughanga@gmail.com>
2021-01-28 09:16:19 +0300

    --allow-empty

--- dist/text.input.js
+++ dist/text.input.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
 
             if ($('#webflow :input[type="text"]')[i]['id'] == e.target.id) {
                 console.log(value, i, $('#mktoForm_1048 :input[type="text"]')[i]);
-                console.log(e.target.value);
-                $('#mktoForm_1048 :input[type="text"]')[i].value = e.target.value;
+                console.log(e.target.value, i);
+                // $('#mktoForm_1048 :input[type="text"]')[i].value = e.target.value;
             }
         });
     })
