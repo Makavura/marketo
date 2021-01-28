452f5a252e60ad54e889045f182a70fe1badff2e
Makavura <makavuramughanga@gmail.com>
2021-01-28 13:22:17 +0300

    --allow-empty

--- dist/final.submission.js
+++ dist/final.submission.js
@@ -6,15 +6,60 @@ $(document).ready(function () {
        */
     $("#webflow-submit").click(function (e) {
         e.preventDefault();
-       
+
+        /* 
+  Get all inputs from first form
+  */
+
+        let initialFormInputs = $("#webflow :input");
+        let mirrorFormInputs = $("#mktoForm_1048 :input");
+
+
+        for (let i = 0; i < initialFormInputs.length; i++) {
+            /* 
+            Access their inner values
+            */
+            console.log("BISCUITS");
+            console.log($(`#${initialFormInputs[i]["id"]}`).attr('type'));
+            console.log($(`#${initialFormInputs[i]["id"]}`));
+            if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'email') {
+                /* 
+                Set email values
+                */
+                $(`#${mirrorFormInputs[i]["id"]}`).attr("value", $(`#${webflow[i]["id"]}`).val());
+            } else if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'text') {
+                /* 
+                Set text values
+                */
+                $(`#${mirrorFormInputs[i]["id"]}`).attr("value", $(`#${webflow[i]["id"]}`).val());
+            } else if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'password') {
+                /* 
+                Set password values
+                */
+                $(`#${mirrorFormInputs[i]["id"]}`).attr("value", $(`#${webflow[i]["id"]}`).val());
+            } else if ($(`#${initialFormInputs[i]["id"]}`).attr('type') == 'number') {
+                /* 
+                Set number values
+                */
+                $(`#${mirrorFormInputs[i]["id"]}`).attr("value", $(`#${webflow[i]["id"]}`).val());
+            }
+
+        }
+        console.log(mirrorFormInputs);
+        console.log(initialFormInputs);
+
+        _mirrorForm = document.getElementsByClassName(_mirrorFormClassName);
+        _mirrorFormData = new FormData(_mirrorForm);
+        console.log(_mirrorFormData);
+
         document.querySelectorAll("textarea").forEach(function (value, index, parent) {
 
             if (index == ((document.querySelectorAll("textarea").length / 2))) {
                 return true;
             }
-    
+
             document.querySelectorAll("textarea")[`${(document.querySelectorAll("textarea").length / 2)}`].value = document.querySelectorAll("textarea")[index].value;
-    
+
         })
         // let _FormData = new FormData($('#mktoForm_1048'));
         // console.log(_FormData);
--- dist/text.input.js
+++ dist/text.input.js
@@ -2,48 +2,48 @@ $(document).ready(function () {
     /* 
      Text based inputs
     */
-    $('#webflow :input[type="text"]').keyup(function (e) {
-        // console.log($(this));
-        /* 
-        For filtering custom setups e.g date pickers
-        Set type in data attribute or in id
-        e.g data-type="date-picker"
-        */
-        $('#webflow :input[type="text"]').each(function (index, value) {
-            /* 
-            Find index of input element
-            Find the element in mirror form that matches this index
-            Mirror selection
-            */
-            /* 
-            If date is included in the id tag of the current element
-            All webflow based date pickers should have an id of date-...
-            Since they are classified as texts and dynamically rendered as date pickers via jquery ui
-            */
-            if ($('#webflow :input[type="text"]')[index]['id'].includes("date")) {
-                $('#mktoForm_1048 :input[type="date"]').each(function (count, value) {
-                    /* 
-                    In event of multiple date pickers
-                    */
-                    if (document.querySelectorAll('[id^=date]').length == (count + 1)) {
-                        $('#mktoForm_1048 :input[type="text"]')[count].value = e.target.value;
-                    }
-                });
-            } else {
-                /* 
-                Does not include date in id
-                Matches current selection
-                */
-                $('#mktoForm_1048 :input[type="text"]').each(function (count, value) {
-                    console.log(count, index, value);                   
-                    if ($('#webflow :input[type="text"]')[index]['id'] == e.target.id) {
-                        if(count == index){
+    // $('#webflow :input[type="text"]').keyup(function (e) {
+    //     // console.log($(this));
+    //     /* 
+    //     For filtering custom setups e.g date pickers
+    //     Set type in data attribute or in id
+    //     e.g data-type="date-picker"
+    //     */
+    //     $('#webflow :input[type="text"]').each(function (index, value) {
+    //         /* 
+    //         Find index of input element
+    //         Find the element in mirror form that matches this index
+    //         Mirror selection
+    //         */
+    //         /* 
+    //         If date is included in the id tag of the current element
+    //         All webflow based date pickers should have an id of date-...
+    //         Since they are classified as texts and dynamically rendered as date pickers via jquery ui
+    //         */
+    //         if ($('#webflow :input[type="text"]')[index]['id'].includes("date")) {
+    //             $('#mktoForm_1048 :input[type="date"]').each(function (count, value) {
+    //                 /* 
+    //                 In event of multiple date pickers
+    //                 */
+    //                 if (document.querySelectorAll('[id^=date]').length == (count + 1)) {
+    //                     $('#mktoForm_1048 :input[type="text"]')[count].value = e.target.value;
+    //                 }
+    //             });
+    //         } else {
+    //             /* 
+    //             Does not include date in id
+    //             Matches current selection
+    //             */
+    //             $('#mktoForm_1048 :input[type="text"]').each(function (count, value) {
+    //                 console.log(count, index, value);                   
+    //                 if ($('#webflow :input[type="text"]')[index]['id'] == e.target.id) {
+    //                     if(count == index){
 
-                            $('#mktoForm_1048 :input[type="text"]')[count].value = e.target.value;
-                        }
-                    }
-                });
-            }
-        });
-    })
+    //                         $('#mktoForm_1048 :input[type="text"]')[count].value = e.target.value;
+    //                     }
+    //                 }
+    //             });
+    //         }
+    //     });
+    // })
 });
