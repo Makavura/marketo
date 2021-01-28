6bcd8a429327b68a0e550fc3a825533125e326d4
Makavura <makavuramughanga@gmail.com>
2021-01-28 15:59:42 +0300

    --allow-empty

--- dist/final.submission.js
+++ dist/final.submission.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
                     if ($('#webflow :input[type="text"]')[index]['id'] == value["id"]) {
                         console.log(index);
                         if(index == 3){
+                            return;
                         }
                         $('#mktoForm_1048 :input[type="text"]')[index].value = value["value"];
                     }
