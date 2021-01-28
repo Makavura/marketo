de025bfa3be2d5283415252cc61fe941bd997b48
Makavura <makavuramughanga@gmail.com>
2021-01-28 10:23:20 +0300

    --allow-empty

--- dist/textarea.input.js
+++ dist/textarea.input.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-
+export default function MirrorTextAreaInputs() {
 
     document.querySelectorAll("textarea").forEach(function (value, index, parent) {
-        value.change(function (e) {
 
-            console.warn(e.target);
+        if (index == ((document.querySelectorAll("textarea").length / 2))) {
+            return true;
+        }
 
-        });
+        document.querySelectorAll("textarea")[`${(document.querySelectorAll("textarea").length / 2)}`].value = document.querySelectorAll("textarea")[index].value;
+
     })
-
-
-});
+    
+};
