64acd3b163cca77cd85a86c09d496f618e71ed5e
Makavura <makavuramughanga@gmail.com>
2021-02-01 22:34:16 +0300

    --prerelease

--- dist/mirror.ii.js
+++ dist/mirror.ii.js
@@ -1,32 +1,13 @@
-/* 
-SETUP INSTRUCTIONS
-
-set custom form inputs with a data attribute to match the id  intended field in webflow 
-e.g for a name entry in marketo with an id of Name, the attribute will be marketo-input-id=Name
-
-For Sliders: set custom form inputs with a data attribute to match intended type e.g for email input set marketo-input-type=slider
-*/
-
 $(document).ready(function () {
-
-    /* 
-    Text Areas
-    */
     textareas = document.getElementsByTagName('textarea');
-
     for (indice = 0; indice < textareas.length; ++indice) {
 
         $(textareas[indice]).change(function (e) {
             document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).value = e.target.value;
         });
-
     }
-    /* 
-    Slider
-    */
-    document.querySelectorAll('[marketo-input-type="slider"]').forEach(function (value, index) {
+    document.querySelectorAll('[marketo-input-type="slider"]').forEach(function (value) {
         $(value).change(function (e) {
-            // console.log(e.target.value, index);
         })
         $(value).slider({
             min: 0,
@@ -36,29 +17,23 @@ $(document).ready(function () {
             }
         });
     });
-    /* 
-    Inputs
-    */
-
+    document.querySelectorAll('[marketo-input-type="date"]').forEach(function (value) {
+        $(value).change(function (e) {
+            document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).value = e.target.value;
+        })
+    });
     inputs = document.getElementsByTagName('input');
-
     for (index = 0; index < inputs.length; ++index) {
-
         if (inputs[index].type == "text") {
-
             $(inputs[index]).keyup(function (e) {
                 document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).value = e.target.value;
             });
-
         } else if (inputs[index].type == "number") {
-
             $(inputs[index]).change(function (e) {
                 console.log(e.target.value);
                 document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).value = e.target.value;
             });
-
         } else if (inputs[index].type == "radio") {
-
             $(inputs[index]).change(function (e) {
                 if (document.getElementById(e.target.id).checked) {
                     const _ = document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id");
@@ -69,15 +44,11 @@ $(document).ready(function () {
                     document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).checked = false;
                 }
             });
-
         } else if (inputs[index].type == "email") {
-
             $(inputs[index]).keyup(function (e) {
                 document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).value = e.target.value;
             });
-
         } else if (inputs[index].type == "checkbox") {
-
             $(inputs[index]).change(function (e) {
                 console.log(e.target.checked);
                 if (e.target.checked) {
@@ -87,7 +58,6 @@ $(document).ready(function () {
                     document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).checked = false;
                 }
             });
-
         } else if (inputs[index].type == "submit") {
 
             $(inputs[index]).click(function (e) {
@@ -95,16 +65,10 @@ $(document).ready(function () {
                 const submissionbuttonid = document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id");
                 document.getElementById(submissionbuttonid).submit();
             });
-
         }
     }
-
-    /* 
-    Select inputs mirrored on change
-    */
     $('select').change(function (e) {
         document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).value = e.target.value;
         document.getElementById(`${document.querySelector(`#${e.target.id}`).getAttribute("marketo-input-id")}`).setAttribute("selected", "selected");
-    })
-
+    });
 })
